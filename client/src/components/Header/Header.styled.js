b04d9e16478d2dc3dc04b1411f79c37124abeb3a
Gustavo Malamud <gustavo.malamud@gmail.com>
2022-09-20 06:10:03 -0300

    Modifications to header

--- client/src/components/Header/Header.styled.js
+++ client/src/components/Header/Header.styled.js
@@ -3,3 +3,12 @@ import styled from 'styled-components';
 export const HeaderContainer = styled.div.attrs(() => ({
   className: 'mb-5 w-full',
 }))``;
+
+export const NavBarContainer = styled.div.attrs(() => ({
+  className:
+    'flex w-full text-left p-5 text-white font-bold bg-gradient-to-r from-black via-violet-500 to-violet-500 background-animate',
+}))``;
+
+export const IconContainer = styled.div.attrs(() => ({
+  className: 'mr-5',
+}))``;
